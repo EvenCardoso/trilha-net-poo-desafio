f86df2cc858769a5a0e75dc70cb982929e2da813
Even Cardoso <even.cardoso@gmail.com>
2024-01-14 14:01:07 -0300

    DESAFIO CONCLUIDO - PROGRAM

--- Program.cs
+++ Program.cs
@@ -1,3 +1,15 @@
 ﻿using DesafioPOO.Models;
 
 // TODO: Realizar os testes com as classes Nokia e Iphone
+
+Console.WriteLine("Smartphone Nokia: ");
+Smartphone nokia = new Nokia(numero: "0111647496", modelo: "Modelo 9", imei: "0011506465", memoria: 128);
+nokia.Ligar();
+nokia.InstalarAplicativo("spotify");
+
+Console.WriteLine("\n");
+
+Console.WriteLine("Smartphone iPhone: ");
+Smartphone iphone = new Iphone(numero: "121113", modelo: "Modelo 8", imei: "971374326", memoria: 128);
+iphone.ReceberLigacao();
+iphone.InstalarAplicativo("Amazon Music");
