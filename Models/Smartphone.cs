c2c90fdaaddb6383f811cb1243961ced486da1ce
Even Cardoso <even.cardoso@gmail.com>
2024-01-14 13:59:29 -0300

    DESAFIO CONCLUIDO - SMARTPHONE

--- Models/Smartphone.cs
+++ Models/Smartphone.cs
@@ -5,20 +5,33 @@ namespace DesafioPOO.Models
         public string Numero { get; set; }
         // TODO: Implementar as propriedades faltantes de acordo com o diagrama
 
-        public Smartphone(string numero)
+        public string Modelo { get; private set; }
+        public string Imei { get; private set; }
+        public int Memoria { get; private set; }
+
+        public Smartphone(string numero, string modelo, string imei, int memoria)
         {
             Numero = numero;
             // TODO: Passar os parâmetros do construtor para as propriedades
+            
+            Modelo = modelo;
+            Imei = imei;
+            Memoria = memoria;
+        }
+
+        protected Smartphone(string numero)
+        {
+            Numero = numero;
         }
 
         public void Ligar()
         {
-            Console.WriteLine("Ligando...");
+            Console.WriteLine($"Ligando para o contato {Numero}.");
         }
 
         public void ReceberLigacao()
         {
-            Console.WriteLine("Recebendo ligação...");
+            Console.WriteLine($"Recebendo ligação do contato {Numero}.");
         }
 
         public abstract void InstalarAplicativo(string nomeApp);
